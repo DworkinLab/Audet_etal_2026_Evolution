80d49774119d5faa7aa22d071892ac4153eb5e8f
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2023-05-23 15:05:30 -0400

    Create make_blast_db.sh

---
+++ make_blast_db.sh
@@ -0,0 +1,5 @@
+makeblastdb -in \
+"/home/audett/projects/def-idworkin/audett/prolongata/genome/D_pro_genome.fasta\
+ /home/audett/projects/def-idworkin/audett/prolongata/genome/carrolli_GCA_018152295.1_ASM1815229v1_genomic.fasta\
+ /home/audett/projects/def-idworkin/audett/prolongata/genome/dmel-all-chromosome-r6.23.fasta"\
+ -title all_genomes -dbtype nucl -out all_genomes.fa
