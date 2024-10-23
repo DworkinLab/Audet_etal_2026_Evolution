9574f2f6cc37ff15cac8d5cb1e3dcd4877a7a72a
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2024-10-23 08:48:21 -0400

    Update bbduk.sh

--- bbduk.sh
+++ bbduk.sh
@@ -1,19 +1,7 @@
-  GNU nano 4.6                        bbduk.sh                                   
-#!/bin/bash
-#SBATCH -t 4:00:00
-#SBATCH -A def-idworkin
-#SBATCH --array=0-451
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=10G
-#SBATCH --mail-user=audett@mcmaster.ca
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
 module load bbmap/38.86
 
-in=/home/audett/projects/def-idworkin/audett/prolongata/RNA
-out=/home/audett/scratch/prolongata/trimmed
+in=RNA
+out=trimmed
 
 declare -a forward=( ${in}/*_R1.fastq.gz )
 
@@ -27,6 +15,6 @@ in1=${in}/${base}_R1.fastq.gz \
 in2=${in}/${base}_R2.fastq.gz \
 out1=${out}/${base}_R1_trimmed.fastq.gz \
 out2=${out}/${base}_R2_trimmed.fastq.gz \
-ref=/home/audett/projects/def-idworkin/audett/prolongata/scripts/adapters.fa \
+ref=genome/adapters.fa \
 threads=32 ftr=101 ktrim=r k=33 mink=15 hdist=1 tpe tbo \
 qtrim=rl trimq=15 minlength=36
