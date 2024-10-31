3bdc275bfc7ca020153efc58d25732a98fb085e5
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2024-10-31 08:38:56 -0400

    Update make_blast_db.sh

--- make_blast_db.sh
+++ make_blast_db.sh
@@ -2,6 +2,6 @@ makeblastdb -in \
 "/prolongata/GCA_036346975.1_ASM3634697v1_genomic.fna\
  /carrolli/GCA_018152295.1_ASM1815229v1_genomic.fna\
  /melanogaster/dmel-all-chromosome-r6.59.fasta"\
- -title all_fly_genomes -dbtype nucl -out all_genomes_db.fa
+ -title all_fly_genomes -dbtype nucl -out /genome/blast_dbs/all_genomes_db.fa
 
 
