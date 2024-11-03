b4cc9209954fc411c377f3627430e17f98c1ae6b
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2024-11-03 12:45:29 -0500

    Update make_fasta_subsets.sh

--- make_fasta_subsets.sh
+++ make_fasta_subsets.sh
@@ -1,23 +1,4 @@
-#!/bin/bash
-#SBATCH -t 2:00:00
-#SBATCH -A def-idworkin
-#SBATCH --array=0-225
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
-#SBATCH --mail-user=audett@mcmaster.ca
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
+module load StdEnv/2020
+module load seqtk/1.3
 
-in=/home/audett/scratch/prolongata/merged_trimmed
-out=/home/audett/scratch/prolongata/merged_trimmed/blast
-
-declare -a forward=( ${in}/*.fastq.gz )
-
-R1=${forward[${SLURM_ARRAY_TASK_ID}]}
-
-name=${R1}
-base=`basename ${name} .fastq.gz`
-
-less ${in}/${base}.fastq.gz | head -n 5000 | sed -n '1~4s/^@/>/p;2~4p' \
-> ${in}/${base}.fasta
+seqtk seq -a ${in}/${base}.fastq.gz > ${out}/${base}.fa
