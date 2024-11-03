6e62bd96ac87fe1d90dee80591fbfe9c87bfeb33
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2024-11-03 12:51:56 -0500

    Update make_fasta_subsets.sh

--- make_fasta_subsets.sh
+++ make_fasta_subsets.sh
@@ -1,4 +1,6 @@
 module load StdEnv/2020
 module load seqtk/1.3
 
-seqtk seq -a ${in}/${base}.fastq.gz > ${out}/${base}.fa
+seqtk sample -s100 ${in}/${base}.fastq.gz 5000 > ${out}/${base}.fq
+
+seqtk seq -a ${out}/${base}.fq > ${out}/${base}.fa
