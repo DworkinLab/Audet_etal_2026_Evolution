a572dc2fd91ce7d96f2fce48b29533a9e52822a9
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2025-11-03 09:12:02 -0500

    Delete make_fasta_subsets.sh

--- make_fasta_subsets.sh
+++
@@ -1,6 +0,0 @@
-module load StdEnv/2020
-module load seqtk/1.3
-
-less merged_trimmed/${base}.fastq.gz | head -n 10000 > fasta_subsets/${base}.fq
-
-seqtk seq -a fasta_subsets/${base}.fq > fasta_subsets/${base}.fa
